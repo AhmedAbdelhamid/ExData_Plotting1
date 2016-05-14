dd62364c91700f6aa86e66cd19ad7c787760aa6f
AHMEDABDELHAMID <cry.try16@hotmail.com>
2016-05-14 18:15:43 +0200

    edit

--- plot4.R
+++ plot4.R
@@ -22,13 +22,13 @@ png(file="plot4.png")
 par(mfrow = c(2,2))
 with(NDt,c(
   plot(DW,Global_active_power ,type = 'l',ylab = "Global Active Power",xlab = ""),
-  plot(DW,Voltage ,type = 'l',ylab = "Voltage",xlab = "datatime"),
+  plot(DW,Voltage ,type = 'l',ylab = "Voltage",xlab = "datetime"),
   plot(DW,Sub_metering_1,type= 'n',xlab= "",ylab ="Energy sub metering"),
   points(DW,Sub_metering_1,type = 'l',ylab = "",xlab = "",col = "black"),
   points(DW,Sub_metering_2,type = 'l',ylab = "",xlab = "",col = "red"),
   points(DW,Sub_metering_3,type = 'l',ylab = "",xlab = "",col = "blue"),
   legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=1,col = c("black","red","blue"),cex = 0.75,bty = 'n'),  
-  plot(DW,Global_reactive_power ,type = 'l',ylab = "Global_reactive_power",xlab = "datatime"))
+  plot(DW,Global_reactive_power ,type = 'l',ylab = "Global_reactive_power",xlab = "datetime"))
 )
 
 
